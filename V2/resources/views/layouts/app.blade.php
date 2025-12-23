27dcad3ecb18687c58933f004a4f597b75099218
xBruno1000x <fariabruno10@gmail.com>
2025-12-22 21:31:13 -0300

    Adiciona preview do site

--- V2/resources/views/layouts/app.blade.php
+++ V2/resources/views/layouts/app.blade.php
@@ -6,6 +6,24 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Lambe Lambe Awards')</title>
     
+    <!-- Open Graph / Facebook / WhatsApp -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:title" content="@yield('og_title', '@yield('title', 'Lambe Lambe Awards')')">
+    <meta property="og:description" content="@yield('og_description', 'Vote nos seus favoritos e celebre o melhor do cinema no Lambe Lambe Awards!')">
+    <meta property="og:image" content="@yield('og_image', asset('images/og-preview.png'))">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:locale" content="pt_BR">
+    <meta property="og:site_name" content="Lambe Lambe Awards">
+    
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:url" content="{{ url()->current() }}">
+    <meta name="twitter:title" content="@yield('og_title', '@yield('title', 'Lambe Lambe Awards')')">
+    <meta name="twitter:description" content="@yield('og_description', 'Vote nos seus favoritos e celebre o melhor do cinema no Lambe Lambe Awards!')">
+    <meta name="twitter:image" content="@yield('og_image', asset('images/og-preview.png'))">
+    
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
